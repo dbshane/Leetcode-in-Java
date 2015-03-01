2f5d078ae83d45b9ca6d6b6d9cc326db17456f00
dbshane <noworry@gmail.com>
2015-03-01 14:36:56 -0500

    note

--- InsertInterval.java
+++ InsertInterval.java
@@ -1,11 +1,12 @@
 import java.util.ArrayList;
 
-
 public class InsertInterval
 {
     public ArrayList<Interval> insert(ArrayList<Interval> intervals, Interval newInterval) {
+    	// great algorithm, the whole point is only when the intervals overlap with newinterval are combined in "temp"
+    	
     	ArrayList<Interval> res = new ArrayList<Interval>();
-    	Interval temp = newInterval;
+    	Interval temp = newInterval;	
     	for (Interval i: intervals)
     	{
     		if (i.end < temp.start)
