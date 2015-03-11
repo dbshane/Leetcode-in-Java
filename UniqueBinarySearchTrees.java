dd4ec70035ff37c97f29ba8aa76148aa8f29e8ad
Sheng <noworry@gmail.com>
2015-03-11 00:36:55 -0400

    Unique Binary Search Trees

---
+++ UniqueBinarySearchTrees.java
@@ -0,0 +1,23 @@
+
+public class UniqueBinarySearchTrees
+{
+    public static int numTrees(int n) {
+        if(n == 0) return 0;
+        if(n == 1) return 1;
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++)
+        {
+            for (int j = 0; j < i/2; j++)
+                dp[i] += (dp[j]*dp[i-j-1])*2;
+            if(i%2 == 1) dp[i] += dp[i/2]*dp[i/2];
+        }
+        return dp[n];
+    }
+    
+    public static void main(String[] args)
+	{
+		System.out.println(numTrees(5));
+	}
+}
